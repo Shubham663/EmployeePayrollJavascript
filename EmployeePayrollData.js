e72734ef122eb3f2502da28042d8ad6cbc8b196b
unknown <mittalshubham05@gmail.com>
2020-11-07 17:12:02 +0530

    Add: added functionality for imposing condition on Name

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -1,50 +1,49 @@
 class EmployeePayrollData{
-    id;
-    name;
-    salary;
-    gender;
-    startDate;
+    _id;
+    _name;
+    _salary;
+    _gender;
+    _startDate;
 
     constructor(id,name,salary,gender,startDate){
         this.id = id;
         this.name = name;
-        this.salary = salary;
-        this.gender = gender;
-        this.startDate = startDate;
+        this._salary = salary;
+        this._gender = gender;
+        this._startDate = startDate;
     }
 
-    get id(){   return this.id; }
+    get id(){   return this._id; }
 
     set id(id){
-        console.log("Setter called");
-        this.id = id;
+        this._id = id;
     }
 
     toString(){
         const options = { year: 'numeric', month: 'long', day: 'numeric'};
-        const empDate = this.startDate === undefined ? "undefined" : this.startDate.toLocaleDateString("en-US",options);
-        return "id= " + this.id + ", name= " + this.name + ", salary= " + this.salary + ", start date= " + empDate + ", gender= " + this.gender;
+        const empDate = this._startDate === undefined ? "undefined" : this._startDate.toLocaleDateString("en-US",options);
+        return "id= " + this.id + ", name= " + this.name + ", salary= " + this._salary + ", start date= " + empDate + ", gender= " + this._gender;
     }
 
-    // set name(name){
-    //     console.log("Setter called for " + this.name);
-    //     this.name = name;
-    // }
-
-    // get name(){
-    //     console.log("getter called");
-    //     return this.name;
-    // }
+    get name(){
+        // console.log("getter called");
+        return this._name;
+    }
+    set name(name){
+        const pattern = RegExp("[A-Z]{1}.{2,}");
+        if(pattern.test(name))
+            this._name = name;
+        else throw "Name does not match the pattern";
+    }
 }
 
-// let employeePayrollData = new EmployeePayrollData();
-// employeePayrollData.id = 10;
-// employeePayrollData.name = "Radhika";
-// employeePayrollData.salary = 320000;
 let employeePayrollData = new EmployeePayrollData(10,"Radhika",320000);
 let employeePayrollData2 = new EmployeePayrollData(15,"Rashmi",500000,"F",new Date(2020,03,01));
 console.log(employeePayrollData.toString());
 console.log(employeePayrollData2.toString());
-
-employeePayrollData.name = "Jaanpal";
-console.log(employeePayrollData.name);
+try{
+    employeePayrollData2.name = "jaanpal";
+}catch(e){
+    console.error(e);
+}
+console.log(employeePayrollData2.toString());
